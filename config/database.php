71c5ba4af803db2502931cbe6729dcda6e0052f9
karim-abdelmegeed <karim.gido@gmail.com>
2020-11-14 03:02:23 +0200

    add database url postgre

--- config/database.php
+++ config/database.php
@@ -67,12 +67,13 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
+            'url'=>$DATABASE_URL,
             'host' => $DATABASE_URL["host"],
             'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"],"/"),
             'username' => $DATABASE_URL["user"],
             'password' =>  $DATABASE_URL["pass"],
-            'charset' => 'utf8mb4',
+//            'charset' => 'utf8mb4',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
