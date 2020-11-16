f6772dda30e87093155abab374aad770b79bd52e
karim-abdelmegeed <karim.gido@gmail.com>
2020-11-17 01:55:16 +0200

    test

--- resources/js/src/index.jsx
+++ resources/js/src/index.jsx
@@ -33,7 +33,7 @@ function App() {
     const hist = createBrowserHistory();
     const isAuthenticated = localStorage.getItem('access_token');
 
-    console.log("systematic test");
+    console.log("test");
     return (
         <div className="app">
             <Router history={hist}>
